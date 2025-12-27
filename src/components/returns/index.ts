07cb4d29a1138b0b7bcbae8e3c862712f46fe2b5
baotran1103 <quocbao207115@gmail.com>
2025-12-27 23:27:30 +0700

    feat: Implement product return and exchange request functionality with dedicated components, routes, types, and service.

---
+++ src/components/returns/index.ts
@@ -0,0 +1,3 @@
+export { default as ReturnRequestForm } from './ReturnRequestForm'
+export { default as MyReturnRequestsList } from './MyReturnRequestsList'
+export { default as ReturnRequestDetail } from './ReturnRequestDetail'
